23d547b9ec1a4fadab7cf4b242ee65948f6c841e
e-guyot <guyot.estellee@gmail.com>
2020-03-03 16:07:55 +0100

    update add documents

--- partiels_old/src/Entity/Documents.php
+++ partiels_old/src/Entity/Documents.php
@@ -15,9 +15,36 @@ class Documents
      * @ORM\Column(type="integer")
      */
     private $id;
+    private $libelle; 
+    private $commentaires; 
+    private $accept; 
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCommentaires()
+    {
+        return $this->commentaires;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getAccept()
+    {
+        return $this->accept;
     }
 }
