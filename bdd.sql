094cba478f6eb88c813987c33fc46e6ae4758553
e-guyot <56032211+e-guyot@users.noreply.github.com>
2020-03-03 14:20:18 +0100

    Update bdd.sql
    Add insert role

--- bdd.sql
+++ bdd.sql
@@ -1,10 +1,10 @@
 create table user (
   id int primary key, 
-  name varchar(255),
+  name varchar(255) NOT NULL,
   username varchar(255),
   mail varchar(255),
-  pwd varchar(255),
-  id_role int,
+  pwd varchar(255) NOT NULL,
+  id_role int NOT NULL,
   FOREIGN KEY (id_role) REFERENCES role(id)
 );
 // pwd ==> password doit être protégé par password_hash
@@ -18,6 +18,12 @@ create table contenu(
  
  create table role (
   id int primary key,
-  libelle varchar(255)
+  libelle varchar(255),
+  desc varchar(255)
  );
   
+insert into role(libelle, desc)  values 
+('ROLE_ADMIN', 'admin, tous les droits'),
+('ROLE_COM', 'communication'),
+('ROLE_REVIEW', 'reviewer'),
+('ROLE_USER', 'membre');
